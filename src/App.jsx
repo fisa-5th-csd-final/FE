bd21dcbfd41101560bf1297ed6672fd46efae863
hyebim <shb010424@gmail.com>
2025-10-15 17:39:53 +0900

    feat: CreatePost, BoardList 화면 연결

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,18 @@ import BoardList from './components/BoardList/BoardList.jsx'
 import './App.css'
 import CreatePost from './components/CreatePost/CreatePost'
 import ViewPost from './components/ViewPost/ViewPost.jsx'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 
 function App() {
   return (
-    <div>
-      <BoardList />
-      <CreatePost />
-      <ViewPost />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<BoardList />} />
+        <Route path="/viewpost" element={<ViewPost />} />
+        <Route path="/createpost" element={<CreatePost />} />
+      </Routes>
+    </Router>
   )
 }
 
