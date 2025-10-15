1e3b5a62e0dd6cdfc8a8e2aa11d72c9b49ee0205
ehgkals <ehgkals12@gmail.com>
2025-10-15 14:58:32 +0900

    ui: 게시물 생성 화면 스타일 설정

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import CreatePost from './components/CreatePost'
+import CreatePost from './components/CreatePost/CreatePost'
 
 function App() {
   return (
